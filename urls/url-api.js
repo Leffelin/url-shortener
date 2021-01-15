1f9276e41d54f33675a978fa2ee0cc3b3cb7c2b0
Magnus Lindhult <magnus.lindhult@gmail.com>
2021-01-15 16:22:21 +0100

    Moved validation to api layer

--- urls/url-api.js
+++ urls/url-api.js
@@ -1,9 +1,66 @@
 const express = require("express");
 const router = express.Router();
+const { body, param, validationResult } = require("express-validator");
 const urlController = require("./url-controller");
 
-router.post("/", urlController.validate("saveUrl"), urlController.saveUrl);
+const validateViewModel = (method) => {
+  switch (method) {
+    case "saveUrl": {
+      return [body("url", "The url to shorten, was not supplied.").exists()];
+    }
+    case "getUrl": {
+      return [
+        param(
+          "id",
+          "The short url to look up, was not supplied in the right format (Number)."
+        )
+          .exists()
+          .isInt(),
+      ];
+    }
+  }
+};
 
-router.get("/:id", urlController.validate("getUrl"), urlController.getUrl);
+router.post("/", validateViewModel("saveUrl"), (req, res) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(422).json({ errors: errors.array() });
+      return;
+    }
+
+    const { url } = req.body;
+    const urlResponse = urlController.saveUrl(url);
+
+    res.json(urlResponse);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.get("/:id", validateViewModel("getUrl"), (req, res) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(422).json({ errors: errors.array() });
+      return;
+    }
+
+    const { id } = req.params;
+    const url = urlController.getUrl(id);
+
+    if (!url) {
+      res.status(404).send("Not found");
+      return;
+    }
+
+    res.set("Location", url);
+    res.status(301).send();
+  } catch (err) {
+    return next(err);
+  }
+});
 
 module.exports = router;
--- urls/url-controller.js
+++ urls/url-controller.js
@@ -1,68 +1,16 @@
-const { body, param, validationResult } = require("express-validator");
 const urlService = require("./url-service");
 
-const validate = (method) => {
-  switch (method) {
-    case "saveUrl": {
-      return [body("url", "The url to shorten, was not supplied.").exists()];
-    }
-    case "getUrl": {
-      return [
-        param(
-          "id",
-          "The short url to look up, was not supplied in the right format (Number)."
-        )
-          .exists()
-          .isInt(),
-      ];
-    }
-  }
+const saveUrl = (url) => {
+  const shortUrl = urlService.saveUrl(url);
+  return { url, shortUrl: `/${shortUrl}` };
 };
 
-const saveUrl = (req, res, next) => {
-  try {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      res.status(422).json({ errors: errors.array() });
-      return;
-    }
-
-    const { url } = req.body;
-    const urlResponse = urlService.saveUrl(url);
-
-    res.json(urlResponse);
-  } catch (err) {
-    return next(err);
-  }
-};
-
-const getUrl = (req, res, next) => {
-  try {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      res.status(422).json({ errors: errors.array() });
-      return;
-    }
-
-    const { id } = req.params;
-    const url = urlService.getUrlById(id);
-
-    if (!url) {
-      res.status(404).send("Not found");
-      return;
-    }
-
-    res.set("Location", url);
-    res.status(301).send();
-  } catch (err) {
-    return next(err);
-  }
+const getUrl = (id) => {
+  const url = urlService.getUrlById(id);
+  return url;
 };
 
 module.exports = {
   getUrl,
   saveUrl,
-  validate,
 };
