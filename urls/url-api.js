c0ad3da9c3c481c255475fb397ec0da3ec2abbb4
Magnus Lindhult <magnus.lindhult@gmail.com>
2021-01-15 16:06:55 +0100

    Introduced express validator

--- urls/url-api.js
+++ urls/url-api.js
@@ -2,34 +2,8 @@ const express = require("express");
 const router = express.Router();
 const urlController = require("./url-controller");
 
-router.post("/", function (req, res) {
-  const data = req.body;
-  if (!data) {
-    res.status(422).send("Need an url in order to create a shortUrl");
-    return;
-  }
+router.post("/", urlController.validate("saveUrl"), urlController.saveUrl);
 
-  const id = urlController.saveUrl(data.url);
-
-  res.status(201).json({ url: data.url, short: `/${id}` });
-});
-
-router.get("/:id", function (req, res) {
-  const id = req.params.id;
-  if (!id) {
-    res.status(404).send();
-    return;
-  }
-
-  const url = urlController.getUrl(id);
-
-  if (!url) {
-    res.status(404).send(`Not found - ${id}`);
-    return;
-  }
-
-  res.set("Location", url);
-  res.status(301).send();
-});
+router.get("/:id", urlController.validate("getUrl"), urlController.getUrl);
 
 module.exports = router;
--- urls/url-controller.js
+++ urls/url-controller.js
@@ -1,10 +1,63 @@
+const { body, param, validationResult } = require("express-validator");
 const urlService = require("./url-service");
 
-const saveUrl = (url) => urlService.saveUrl(url);
+const validate = (method) => {
+  switch (method) {
+    case "saveUrl": {
+      return [body("url", "The url to shorten, was not supplied.").exists()];
+    }
+    case "getUrl": {
+      return [
+        param(
+          "id",
+          "The short url to look up, was not supplied in the right format (Number)."
+        )
+          .exists()
+          .isInt(),
+      ];
+    }
+  }
+};
 
-const getUrl = (id) => urlService.getUrlById(id);
+const saveUrl = (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(422).json({ errors: errors.array() });
+      return;
+    }
+
+    const { url } = req.body;
+    const urlResponse = urlService.saveUrl(url);
+
+    res.json(urlResponse);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+const getUrl = (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(422).json({ errors: errors.array() });
+      return;
+    }
+
+    const { id } = req.params;
+    const url = urlService.getUrlById(id);
+
+    res.set("Location", url);
+    res.status(301).send();
+  } catch (err) {
+    return next(err);
+  }
+};
 
 module.exports = {
   getUrl,
   saveUrl,
+  validate,
 };
